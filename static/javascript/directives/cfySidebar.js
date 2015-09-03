13321d0d754da023bc2a5c1366154c7736fbb999
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-09-03 18:20:45 +0300

    fix(footer): make the footer stick to bottom
    chose a javascript solution... I hate those in general, but this time seemed more suitable.

--- static/javascript/directives/cfySidebar.js
+++ static/javascript/directives/cfySidebar.js
@@ -1,6 +1,6 @@
 // this directive helps us display the sidebar in collision with the footer.
 // when the footer is displayed, we shorten the sidebar.
-angular.module('getcloudify').directive('cfySidebar', function(   ){
+angular.module('getcloudify').directive('cfySidebar', function(  ){
 
     return {
         restrict: 'C',
@@ -15,21 +15,54 @@ angular.module('getcloudify').directive('cfySidebar', function(   ){
                     return $(window).width() > 768;
                 }
             };
+            // also fix footer position here
+
+            function fixFooterPosition(){
+                var current = $('.footer').position().top;
+                var proper = $(window).height() - $('.footer').outerHeight();
+
+                console.log('proper,current', proper, current );
+                if ( current < proper ){
+                    $('.footer').css( { 'position' : 'fixed' , 'bottom' : 0 });
+                }else{
+                    $('.footer').css( { 'position' : 'inherit' , 'bottom' : 'initial' });
+                }
+
+            }
+
 
             var autoFitSidebar = function () {
-                if (screenBigEnough()) {
-                    // Find the offset top of the bottom of the screen
-                    var windowTop = $(window).scrollTop();
-                    var footerTop = $('.footer').offset().top;
-                    var windowBottom = windowTop + $(window).height();
-                    $(element).css('bottom',Math.max(0, windowBottom - footerTop ));
+                console.log('auto fit sidebar');
+                fixFooterPosition();
+                try {
+                    if (screenBigEnough()) {
+                        // Find the offset top of the bottom of the screen
+                        var windowTop = $(window).scrollTop();
+                        var footerTop = $('.footer').offset().top;
+                        var windowBottom = windowTop + $(window).height();
+                        $(element).css('bottom', Math.max(0, windowBottom - footerTop));
+                    }
+                }catch(e){
+                    // fail silently
                 }
 
             };
 
             $(window).scroll(autoFitSidebar);
 
+            $(window).resize(autoFitSidebar);
 
+            $scope.$watch(function(){
+                try {
+                    console.log($('.footer').offset().top);
+                    return $('.footer').offset().top;
+                }catch(e){
+                    return null;
+                }
+            } , autoFitSidebar); // does not work with zoom in/out
+
+
+                //$interval(fixFooterPosition,1000);
             autoFitSidebar();
 
         }
