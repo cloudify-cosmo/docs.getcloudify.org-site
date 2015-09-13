a339c8e7dff0bc6daee521ff8935f80ad59feb8c
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-09-13 12:07:08 +0300

    remove debugger.

--- static/javascript/directives/cfyLinklist.js
+++ static/javascript/directives/cfyLinklist.js
@@ -6,10 +6,9 @@ angular.module('getcloudify').directive('gsLinklist', function(  ){
             'selector' : '@gsLinklist'
         },
         template: '<ul><li ng-repeat="item in items"><a href="#{{item.id}}">{{item.text}}</a></li></ul>',
-        link: function ($scope, element) {
+        link: function ($scope) {
             $scope.items = [];
             $scope.$watch( function(){return $($scope.selector).length; }, function(){
-                debugger;
                 $($scope.selector).each(function(index,elem){
                     var $elem = $(elem);
                     $scope.items.push({ 'id' : $elem.attr('id') , 'text' : $elem.text() });
